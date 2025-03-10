ec25163083427d313d8f6d7dbc6f6291f7ac0d83
KennSol <kennethsolorzano113@gmail.com>
2025-03-10 11:58:59 -0700

    c13

--- TablasdeVerdad.py
+++ TablasdeVerdad.py
@@ -18,13 +18,13 @@ def bicondicional(p,q,r=None):
         return p == q == r  
 
 def disyuncion_excluyente (p,q,r=None):
-    if r is None:
-        return not(p==q)
+    if c is None:
+        return p != q  
     else:
-        return not(p == q == r)
+        return (p + q + r) % 2 != 0
 
 def tabla(conector,nombre,c):
-    print(f"Tabla de verdad para {nombre}:")
+    print(f"///Tabla de verdad para {nombre}:///")
     if c==1:
         print(f"| p |    {nombre}")
         print("--+--------------")
@@ -55,7 +55,7 @@ def tabla(conector,nombre,c):
 
 
 def menu ():
-    print("*Tablas de Verdad*")
+    print("///Tablas de Verdad///")
     time.sleep(0.5)
     print("""***Menu de Opciones*** 
     1. Generar Tablas de Verdad
@@ -63,18 +63,17 @@ def menu ():
     3. Salir""")
 
 def menu2 ():
-    print("*Tablas de Verdad*")
-    time.sleep(0.5)
     print("""**Expresiones Logicas**
     1. Conjuncion
     2. Disyuncion
     3. Condicional
     4. Bicondicional
-    5. Disyuncion Excluyente""")
+    5. Disyuncion Excluyente
+    6. Regresar a Menu Principal""")
 op=0
 variable=""
 nombre=""
-c=0
+c=2
 while True:
     menu()
     op=int(input("Opcion: "))
@@ -83,66 +82,74 @@ while True:
             if c < 1:
                 print("!NO HA INGRESADO VARIABLES!")
             else:
-                menu2()
-                op=int(input("Expresion a Ingresar: "))
-                match op:
-                    case 1:
-                        if c==1:
-                            nombre="p^p"
-                            tabla(conjunción,nombre,1)
-                        elif c==2:
-                            nombre="p^q"
-                            tabla(conjunción,nombre,2)
-                        else:
-                            nombre="(p^q)^r"
-                            tabla(conjunción,nombre,3)
-                    case 2:
-                        if c==1:
-                            nombre="pvp"
-                            tabla(disyuncion,nombre,1)
-                        elif c==2:
-                            nombre="pvq"
-                            tabla(disyuncion,nombre,2)
-                        else:
-                            nombre="(pvq)vr"
-                            tabla(disyuncion,nombre,3)
-                    case 3:
-                        if c==1:
-                            nombre="p→p"
-                            tabla(condicional,nombre,1)
-                        elif c==2:
-                            nombre="p→q"
-                            tabla(condicional,nombre,2)
-                        else:
-                            nombre="(p→q)→r"
-                            tabla(condicional,nombre,3)
-                    case 4:
-                        if c==1:
-                            nombre="p↔p"
-                            tabla(bicondicional,nombre,1)
-                        elif c==2:
-                            nombre="p↔q"
-                            tabla(bicondicional,nombre,2)
-                        else:
-                            nombre="(p↔q)↔r"
-                            tabla(bicondicional,nombre,3)
-                    case 5:
-                        if c==1:
-                            nombre="p⊻p"
-                            tabla(disyuncion_excluyente,nombre,1)
-                        elif c==2:
-                            nombre="p⊻q"
-                            tabla(disyuncion_excluyente,nombre,2)
-                        else:
-                            nombre="(p⊻q)⊻r"
-                            tabla(disyuncion_excluyente,nombre,3)
-                    case _:
-                        print("!NO VALIDO!")
+                while op==1:
+                    menu2()
+                    op=int(input("Expresion a Ingresar: "))
+                    match op:
+                        case 1:
+                            if c==1:
+                                nombre="p^p"
+                                tabla(conjunción,nombre,1)
+                            elif c==2:
+                                nombre="p^q"
+                                tabla(conjunción,nombre,2)
+                            else:
+                                nombre="(p^q)^r"
+                                tabla(conjunción,nombre,3)
+                        case 2:
+                            op=1
+                            if c==1:
+                                nombre="pvp"
+                                tabla(disyuncion,nombre,1)
+                            elif c==2:
+                                nombre="pvq"
+                                tabla(disyuncion,nombre,2)
+                            else:
+                                nombre="(pvq)vr"
+                                tabla(disyuncion,nombre,3)
+                        case 3:
+                            op=1
+                            if c==1:
+                                nombre="p→p"
+                                tabla(condicional,nombre,1)
+                            elif c==2:
+                                nombre="p→q"
+                                tabla(condicional,nombre,2)
+                            else:
+                                nombre="(p→q)→r"
+                                tabla(condicional,nombre,3)
+                        case 4:
+                            op=1
+                            if c==1:
+                                nombre="p↔p"
+                                tabla(bicondicional,nombre,1)
+                            elif c==2:
+                                nombre="p↔q"
+                                tabla(bicondicional,nombre,2)
+                            else:
+                                nombre="(p↔q)↔r"
+                                tabla(bicondicional,nombre,3)
+                        case 5:
+                            op=1
+                            if c==1:
+                                nombre="p⊻p"
+                                tabla(disyuncion_excluyente,nombre,1)
+                            elif c==2:
+                                nombre="p⊻q"
+                                tabla(disyuncion_excluyente,nombre,2)
+                            else:
+                                nombre="(p⊻q)⊻r"
+                                tabla(disyuncion_excluyente,nombre,3)
+                        case 6:
+                            op=0
+                        case _:
+                            print("!NO VALIDO!")
+                            op=1
         case 2:
             if c < 3:
                 variable=input("Ingrese la Variable: ")
                 c=c+1
-            elif c > 3:
+            else:
                 print("!MAXIMO DE VARIABLES ALCANZADO!")
         case 3:
             print("Saliendo en")
